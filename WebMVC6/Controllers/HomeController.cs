7b410f56f8bf2b7b5d73483188010fa306428533
shanghaik168@gmail.com <Shanghaik1998>
2023-07-25 13:15:10 +0700

    ayza

--- WebMVC6/Controllers/HomeController.cs
+++ WebMVC6/Controllers/HomeController.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebMVC6.Models;
+using WebMVC6.Service.Services;
 
 namespace WebMVC6.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        // private readonly ILogger<HomeController> _logger;
+        private readonly ProductServices _services;
+        public HomeController()
         {
-            _logger = logger;
+            _services= new ProductServices();
         }
 
         public IActionResult Index()
@@ -31,6 +32,25 @@ namespace WebMVC6.Controllers
         }
         public IActionResult ChangeDirection() {
             return RedirectToAction("Test");
+        }
+
+        public IActionResult GetAllProduct()
+        {
+            // Gen view tự đông 
+            // Chuột phải: Chọn add View => Razor View
+            // Thiết lập: Tên giữ nguyên, Chọn template là List
+            // CHọn Model là Model mình muốn
+            // Faked List
+            List<Product> products = new List<Product>()
+            {
+                new Product{ Name = "Thầy Việt", Id = Guid.NewGuid() },
+                new Product{ Name = "Thầy Khánh", Id = Guid.NewGuid() },
+                new Product{ Name = "Thầy Dũng", Id = Guid.NewGuid() },
+                new Product{ Name = "Thầy Tiến", Id = Guid.NewGuid() },
+                new Product{ Name = "Thầy Đan", Id = Guid.NewGuid() },
+            };
+            return View(products); // Truyền thẳng Models vào View
+            // Mỗi View chỉ có duy nhất 1 Model đại diện
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
